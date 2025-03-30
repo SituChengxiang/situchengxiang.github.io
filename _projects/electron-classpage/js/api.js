f2c80a9bc239f2d0eccead94f22d6fa4c246768f
SituChengxiang <kuan896048176@outlook.com>
2025-03-30 16:26:58 +0800

    很多东西挪了一下(新建Projects)

---
+++ _projects/electron-classpage/js/api.js
@@ -0,0 +1,61 @@
+// 加载必应图片介绍
+function loadBingIntroduce() {
+  fetch('https://fly.atlinker.cn/api/bing/cn.php?q=copyright')
+    .then(response => response.text())
+    .then(data => {
+      const introduce = document.querySelector('#introduce');
+      introduce.innerText = '今天的 Bing 每日一图：' + data
+    })
+    .catch(console.error);
+}
+
+// 金山词霸每日一句
+function handleData(data) {
+  console.log(data);
+  if (typeof data !== "undefined" && typeof data.content === "string") {
+    document.getElementById("content_span").innerHTML = data.content;
+  }
+  if (typeof data !== "undefined" && typeof data.note === "string") {
+    document.getElementById("note_span").innerHTML = data.note;
+  }
+}
+
+// 生日列表
+function loadBirthdays() {
+  //声明数组储存同学们的生日和日期
+  var names = ['Mike', 'Boppy', 'Carter', 'Bruce', 'Sam', 'Chris', 'Adam', 'Frank', '胡俊洋', '胡峻浩', 'Andy', 'Justin', '蒋京', 'Lan', 'Colin', 'Gino', 'Sabber', 'Tim', '毛毅涛', 'Alan', 'Jeb, Ray', 'Max', 'Glock', 'Tom', 'Shaper', 'Hawk', '徐翊博', 'Grant', 'Chuman'];
+  var birthdays = ['09.25', '12.21', '10.14', '11.19', '08.20', '04.26', '06.09', '12.02', '03.02', '01.28', '12.22', '01.08', '01.23', '03.11', '05.28', '08.11', '04.29', '10.06', '05.18', '09.24', '01.17', '01.11', '06.10', '06.03', '07.17', '10.27', '11.09', '10.29', '10.01']
+  // 获取当前日期和月份
+  var results = "",
+      flag = false;
+  for (var i = 0; i < names.length; i++) {
+      var dt = "";
+      if (new Date().getMonth() > 8) dt = (new Date().getMonth() + 1) + ".";
+      else dt = "0" + (new Date().getMonth() + 1) + ".";
+      if (new Date().getDate() > 9) dt = dt + new Date().getDate();
+      else dt = dt + "0" + new Date().getDate();
+      if (birthdays[i] == dt) {
+          results = results + names[i] + ",";
+          flag = true;
+      }
+  }
+  if (flag) {
+      results = results.substring(0, results.length - 1) + " " + "生日快乐🎂🎂！！！";
+  } else {
+      results = "今天班里没人生日呐";
+  }
+  document.getElementById("birthday-div").innerHTML = (results);
+}
+
+// 初始化页面
+window.onload = function() {
+  loadBingIntroduce();
+  loadBirthdays();
+  
+  // 加载金山词霸API
+  (function () {
+    var script = document.createElement('script');
+    script.src = 'https://open.iciba.com/dsapi/?callback=handleData';
+    document.body.appendChild(script);
+  })();
+};
--- pages/about.md
+++ pages/about.md
@@ -16,24 +16,25 @@ Email: <a href="mailto:lk@atlinker.cn">lk@atlinker.cn</a>
 
 Github: [link9596](https://github.com/link9596)
 
+
 ## #网站拥有者
 
 Emmm，我还没有想好写些啥好  
 
-浙江省双非学校在读学生，目前只想“survive the four years”，然后去个好大学。
+浙江省双非学校在读学生，目前只想"survive the four years"，然后去个好大学。
 
 高中时期的一些遗存（貌似有BUG，等我想好了再修）：
 
-- [Yan群的URL](https://situchengxiang.github.io/pages/usefulurls.html)
+- [Yan群的URL](https://situchengxiang.github.io/_projects/usefulurls.html)
 
-- [XSB2409-效实24届白杨9班的班级主页](https://situchengxiang.github.io/pages/xsb2409.html)
+- [XSB2409-效实24届白杨9班的班级主页](https://situchengxiang.github.io/_projects/electron-classpage/xsb2409.html)
 
 ↑ 这是我在高中时期为班级开发的一个网页项目，展示了班级的信息、活动和特色。
 
 ### 功能
 
 - **首考高考倒计时**: 自动计算距离首考和高考的剩余时间
-- **Bing每日一图**: 用别人的接口获取Bing搜索每日更新的精美图片
+- **Bing每日一图**: 用linker的接口获取Bing搜索每日更新的精美图片
 - **金山词霸每日一句**: 展示中英双语的每日励志句子
 - **生日提醒**: 自动检查并提醒班级同学的生日
 - **天气预报**: 集成第三方天气服务
